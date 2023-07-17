9dfde4efd076a9d3b344c1fd39b4c9bc45565509
developer2828 <harold.llames@aiqueinnovation.com>
2023-07-17 13:46:59 +0800

    added flying from and to UI

--- src/app/pages/flights/book-a-flight/book-a-flight.page.ts
+++ src/app/pages/flights/book-a-flight/book-a-flight.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-a-flight',
@@ -9,7 +10,9 @@ export class BookAFlightPage implements OnInit {
 
   tripType = 'one-way'
   isLoading = false;
-  constructor() { }
+  flyingFrom = ''
+  flyingTo = ''
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +23,18 @@ export class BookAFlightPage implements OnInit {
 
   changeTripType(type: any) {
     this.tripType = type
+  }
+
+  openFromToPage(type: any) {
+    let query = {}
+    if (type == 'from') query = { type: type, location: this.flyingFrom, tripType: this.tripType }
+    else query = { type: type, location: this.flyingTo, tripType: this.tripType }
+
+    const params: NavigationExtras = {
+      queryParams: query
+    }
+
+    this.router.navigate(['/flying-from-to'], params)
   }
 
 }
